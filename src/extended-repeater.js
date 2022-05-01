8909e2cfcf5dd41d9568cf640ef89a13c9d65756
Xtcnz181988 <koblenev88@gmail.com>
2022-05-01 23:24:28 +0300

    1

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const objDefault= {
+    repeatTimes: 1, 
+    separator: '+', 
+    addition: '', 
+    additionSeparator: '|',
+    additionRepeatTimes: 1,
+  }
+  const resArr = [];
+  for (let key in options) {
+    objDefault[key] = options[key]
+  }
+  for (i = 1; i <= objDefault.additionRepeatTimes; i++) {
+    resArr.push(`${objDefault.addition}`);
+  }
+  console.log(resArr);
+  console.log(resArr.join(`${objDefault.additionSeparator}`));
+  let resStr = `${str}` + resArr.join(`${objDefault.additionSeparator}`);
+  // console.log(resStr);
+  let res = resStr;
+  for (i = 1; i < objDefault.repeatTimes; i++) {
+    // console.log(resStr);
+    res = res + objDefault.separator + resStr;
+    // console.log(res);
+  }
+  // console.log(res)
+  return res;
+  //   if(!options.hasOwnProperty('separator')) {
+  //     console.log('нету');
+  //     return resArr.join('+');
+  //   } else {
+  //     console.log('есть')
+  //     return resArr.join(`${options.separator}`)
+  //   }
+  // } else {
+  //   const resArr = [];
+
+  // }
+    
+  // // const arr = [];
+  // // for (let i = 1; i < options.additionRepeatTimes; i++) {
+  // //   arr.push(options.addition)
+  // // }
+  // // const arr1 = [];
+  // }
 }
 
 module.exports = {
   repeater
 };
+repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' });
